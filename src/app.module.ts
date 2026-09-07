b1cc72074b3b0db46902e137a2cfb11b160bb9a0
nikolaenko <aleshka.dev@gmail.com>
2026-09-07 16:29:32 +0300

    feat: add Redis module and integrate ioredis dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from "@nestjs/config";
 import { ThrottlerModule } from "@nestjs/throttler";
 import { validateEnv } from "./common/config/env.validation";
 import { DrizzleModule } from "./infrastructure/database/database.module";
+import { RedisModule } from "./infrastructure/redis/redis.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate: validateEnv }),
     ThrottlerModule.forRoot([{ ttl: 60_000, limit: 100 }]),
     DrizzleModule,
+    RedisModule,
   ],
 })
 export class AppModule {}
---
+++ src/infrastructure/redis/redis.module.ts
@@ -0,0 +1,20 @@
+import { Global, Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import Redis from "ioredis";
+
+export const REDIS_CLIENT = Symbol("REDIS_CLIENT");
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: REDIS_CLIENT,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return new Redis(configService.getOrThrow<string>("REDIS_URL"));
+      },
+    },
+  ],
+  exports: [REDIS_CLIENT],
+})
+export class RedisModule {}
