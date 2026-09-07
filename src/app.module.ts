b5ad16eeaa01d32707a86ffbe84bbb2424bfc33e
nikolaenko <aleshka.dev@gmail.com>
2026-09-07 16:22:16 +0300

    feat: add Drizzle ORM with PostgreSQL database infrastructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ThrottlerModule } from "@nestjs/throttler";
 import { validateEnv } from "./common/config/env.validation";
+import { DrizzleModule } from "./infrastructure/database/database.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, validate: validateEnv }),
     ThrottlerModule.forRoot([{ ttl: 60_000, limit: 100 }]),
+    DrizzleModule,
   ],
 })
 export class AppModule {}
