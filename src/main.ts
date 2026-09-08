5fa3ee7a9184e549bc8477a58ef2501a0405e47f
nikolaenko <aleshka.dev@gmail.com>
2026-09-08 11:14:31 +0300

    feat(common): add Drizzle exception filter

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ import helmet from 'helmet';
 
 import type { Env } from './common/config/env.validation';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { DrizzleExceptionFilter } from './common/filters/drizzle-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -24,7 +25,7 @@ async function bootstrap() {
     }),
   );
 
-  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalFilters(new AllExceptionsFilter(), new DrizzleExceptionFilter());
 
   app.use(helmet());
   app.use(cookieParser());
